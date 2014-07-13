965284968eaedf751f48e6ba82e6e99b2fd0535d
Jaspalsinh Chauhan <jachauhan@gmail.com>
2014-07-13 20:46:01 +0530

    Added animation and other links

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+/**
+ * Contains all site wide scripts
+ */
+
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        $('#name').addClass('animated bounceInLeft');
+
+        $('#sub-text').addClass('animated fadeInUp');
+
+        $('#social-fa').addClass('animated bounceInRight');
+
+    });
+
+    /*$(window).load(function(){
+    });*/
+
+}(jQuery));
